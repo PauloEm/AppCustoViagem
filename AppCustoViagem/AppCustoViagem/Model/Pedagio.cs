af823bd4c83b6ad8f46bd9d99f82bc92b99e2ac2
Usuario <Usuario@CPD02-M03>
2022-09-06 11:54:28 -0300

    Adicionar arquivos de projeto.

---
+++ AppCustoViagem/AppCustoViagem/Model/Pedagio.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCustoViagem.Model
+{
+    public class Pedagio
+    {
+
+        string _localização;
+        double _valor;
+
+        public string Localização
+        {
+            get => _localização;
+            set
+            {
+                if (value == null)
+                    throw new Exception("Coloque a sua Localização");
+                        _localização = value;
+
+            }
+        }
+        public double Valor
+        {
+            get => _valor;
+            set
+            {
+                if (value == 0)
+                    throw new Exception("Coloque o valor");
+                _valor = value;
+            }
+        }
+    }
+}
